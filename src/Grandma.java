2931f7eb94d45f2d05f0cf08380194316672be71
Michael <martin.michael1477@gmail.com>
2024-09-07 14:07:15 -0500

    stubbed out Grandma class

---
+++ src/Grandma.java
@@ -0,0 +1,15 @@
+
+public class Grandma {
+
+	public boolean canGrandmaHearyou(String phrase) {
+		boolean canSheHearYou = false; 
+		// logic to determine if she can hear you 
+		return canSheHearYou; 
+	}
+	
+	public String grandmaResponse() {
+		String response = ""; 
+		// logic to determine response 
+		return response; 
+	}
+}
