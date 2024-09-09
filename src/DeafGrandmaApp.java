843b86a95e3b6b7c4566f9b36a5ddb085f442cad
Michael <martin.michael1477@gmail.com>
2024-09-09 17:12:01 -0500

    User story 1 and 2 Complete

--- src/DeafGrandmaApp.java
+++ src/DeafGrandmaApp.java
@@ -1,18 +1,36 @@
+import java.util.Scanner;
 
 public class DeafGrandmaApp {
 
 	public static void main(String[] args) {
 		DeafGrandmaApp app = new DeafGrandmaApp();
-
 		app.launch();
 
 	}
 
 	private void launch() {
-		// Scanner 
-		// User Prompt 
-		// Instantiate Grandma 
-		// Bulk of where logic will be
-	}
+		// Scanner
+		Scanner sc = new Scanner(System.in);
 
+		// User Prompt
+		System.out.println("Say something to Grandma: ");
+		String grandsonInput = sc.nextLine();
+	
+
+		System.out.println("You said: " + grandsonInput);
+
+		// Instantiate Grandma
+		Grandma grandma = new Grandma(); 
+		boolean canSheHearYou = grandma.canGrandmaHearyou(grandsonInput); 
+		
+		if(canSheHearYou) {
+			System.out.println("NO... NOT TODAY SONNY. GRANDMA IS TIRED");
+		} else {
+			System.out.println("HUH JOHNNY??? SPEAK UP I CAN'T HEAR YOU!");
+		}
+		
+
+		// Bulk of where logic will be
+
+	}
 }
--- src/Grandma.java
+++ src/Grandma.java
@@ -1,15 +1,19 @@
+import java.util.Scanner;
 
 public class Grandma {
 
 	public boolean canGrandmaHearyou(String phrase) {
-		boolean canSheHearYou = false; 
-		// logic to determine if she can hear you 
-		return canSheHearYou; 
+		boolean canSheHearYou = false;
+		// logic to determine if she can hear you
+		return phrase.equals(phrase.toUpperCase());
+
 	}
-	
+
 	public String grandmaResponse() {
-		String response = ""; 
-		// logic to determine response 
-		return response; 
+		String response = "";
+
+		// logic to determine response
+
+		return response;
 	}
 }
